c566595387cba862c19b04c57369fd73a67b3391
SupuniAbeysinghe <121711723+SupuniAbeysinghe@users.noreply.github.com>
2023-07-22 11:43:45 +0530

    Main.java v2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,52 @@
-import com.sun.security.jgss.GSSUtil;
-
 import java.util.Scanner;
 
 public class Main {
+
+
+    public static Scanner sc = new Scanner(System.in);
+    static DBoperation objDBO = new DBoperation();
+
     public static void main(String[] args) {
 
+
+        boolean isRunning = true;
+        while (isRunning) {
+            showOptions();
+
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addBooks();
+                    break;
+                case 2:
+                    //borrowBook(scanner);
+                    break;
+                case 3:
+                    Removebook();
+                    break;
+                case 4:
+                    //bookAdd(scanner);
+                    break;
+
+                case 5:
+                    //lendingbook();
+                    break;
+
+                case 6:
+                    //bookAdd(scanner);
+                    break;
+                case 7:
+                    System.out.println("Exiting Library Management System.");
+                    sc.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+
+
+    }
+    public static void showOptions () {
         System.out.println("Welcome to the Library Mnagemnt System UOK!");
         System.out.println("Please choose an option");
         System.out.println("1.Add a new book.");
@@ -14,43 +56,48 @@ public class Main {
         System.out.println("5.eturn a book.");
         System.out.println("6.List all avaliable book.");
         System.out.println("7.Exit");
+    }
 
-        Scanner scanner = new Scanner(System.in);
+    public static void addBooks(){
 
+        Books books = new Books();
+        System.out.println("----Add a new book----");
+        System.out.println("Enter the isbn :");
+        books.setIsbn(sc.nextInt());
+        System.out.println("Enter the Title : ");
+        sc.nextLine();
+        books.setTitle(sc.nextLine());
+        System.out.println("Enter the Category : ");
+        books.setCategory(sc.nextLine());
+        System.out.println("Enter the Author : ");
+        books.setAuthor(sc.nextLine());
 
-        int choice = scanner.nextInt();
-        scanner.nextLine();
+        objDBO.addBook(books);
 
-        switch (choice) {
-            case 1:
-//                displayAllBooks();
-//                library.displayAvailableBooks();
-                break;
-            case 2:
-                //borrowBook(scanner);
-                break;
-            case 3:
-                //returnBook(scanner);
-                break;
-            case 4:
-                //bookAdd(scanner);
-                break;
-
-            case 5:
-                //bookAdd(scanner);
-                break;
-
-            case 6:
-                //bookAdd(scanner);
-                break;
-            case 7:
-                System.out.println("Exiting Library Management System.");
-                scanner.close();
-                System.exit(0);
-            default:
-                System.out.println("Invalid choice. Try again.");
-
-        }
 
     }
+
+    public static void Removebook(){
+        System.out.println("Remove and existing book.");
+        System.out.println("REnter isbn number.");
+        int x =sc.nextInt();
+        System.out.println("\nAre you sure remove this book(y/n): ");
+        String confirm = sc.next();
+        if(confirm.equals("y")){
+            objDBO.remove(x);
+        }else{
+            System.out.println("Did not clear data!");
+        }
+    }
+
+    public static void lendingbook(){
+        System.out.println("Lending a book.");
+        System.out.println("REnter isbn number.");
+        int x =sc.nextInt();
+        objDBO.lend(x);
+    }
+
+
+
+}
 }
